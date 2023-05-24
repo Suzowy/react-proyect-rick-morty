81871dec801c6a2feb0a9aea8b670f00ea661921
Suzowy <suzowy@gmail.com>
2023-05-24 19:44:21 +0200

    video

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -9,9 +9,9 @@ const About = () => {
       <p>Rick Sánchez es un ejemplo del típico "científico loco". Es un genio, pero es irresponsable, alcohólico, egoísta, un poco depresivo y con poca cordura. Rick por diferentes razones termina mudándose a la casa de su hija Beth y en ese momento se encuentra con su nieto Morty; un chico de 14 años de edad, tímido y no muy listo. Al juntarse con su nieto, Rick y Morty viven una variedad de aventuras a lo largo del cosmos y universos paralelos. Y es mediante tantas vivencias y reflexiones que Rick busca que su nieto Morty no acabe como su padre, Jerry, un hombre muy poco exitoso que a pesar de tener buenas intenciones resulta ser bastante inútil en muchas ocasiones y depende mucho de su esposa, Beth, hija de Rick.
         A pesar de estar muy apegados, Rick y su nieto experimentan momentos en los que Summer, hermana de Morty, se une en ocasiones a las pintorescas aventuras provocadas por Rick.</p>
         <h2>Aqui puedes ver la intro</h2>
-        <div className='trailer'>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/E6TUs69Cw94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
+        
+        <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/E6TUs69Cw94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+       
 
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import "./Home.scss"
 const Home = () => {
   return (
+    
     <div className='home'>
-        <img src="" alt="imagen"/>
+    
+        <img src="https://4kwallpapers.com/images/wallpapers/rick-and-morty-rick-3840x1080-9531.jpg" alt="imagen"/>
     </div>
   )
 }
