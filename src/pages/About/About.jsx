3b00351dd275f0e4dcc055767e0bae1d0b7a09bb
Suzowy <suzowy@gmail.com>
2023-05-24 19:10:36 +0200

    css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,10 +3,15 @@ import "./About.scss"
 const About = () => {
   return (
     <div className='about'>
-      <p class="p1">Rick y Morty es una serie de televisión estadounidense de animación para adultos creada por Justin Roiland y Dan Harmon en 2013 para Adult Swim, también se emitió en Cartoon Network.</p>
-      <iframe width="560" height="315" className='video' src="https://www.youtube.com/embed/E8cXKMR9a1Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen ></iframe>
+    <h2 className='h2'>Argumento</h2>
+      <p>Rick y Morty es una serie de televisión estadounidense de animación para adultos creada por Justin Roiland y Dan Harmon en 2013 para Adult Swim, también se emitió en Cartoon Network.</p>
+      
       <p>Rick Sánchez es un ejemplo del típico "científico loco". Es un genio, pero es irresponsable, alcohólico, egoísta, un poco depresivo y con poca cordura. Rick por diferentes razones termina mudándose a la casa de su hija Beth y en ese momento se encuentra con su nieto Morty; un chico de 14 años de edad, tímido y no muy listo. Al juntarse con su nieto, Rick y Morty viven una variedad de aventuras a lo largo del cosmos y universos paralelos. Y es mediante tantas vivencias y reflexiones que Rick busca que su nieto Morty no acabe como su padre, Jerry, un hombre muy poco exitoso que a pesar de tener buenas intenciones resulta ser bastante inútil en muchas ocasiones y depende mucho de su esposa, Beth, hija de Rick.
         A pesar de estar muy apegados, Rick y su nieto experimentan momentos en los que Summer, hermana de Morty, se une en ocasiones a las pintorescas aventuras provocadas por Rick.</p>
+        <h2>Aqui puedes ver la intro</h2>
+        <div className='trailer'>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/E6TUs69Cw94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
 
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.scss"
 const Home = () => {
   return (
     <div className='home'>
-        <img src="https://i.pinimg.com/originals/61/de/2b/61de2ba956418acd9ab454ceca15bbb2.jpg" alt="imagen"/>
+        <img src="" alt="imagen"/>
     </div>
   )
 }
